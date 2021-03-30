22e62b4c234d3f9d0895762a8ccc36faff0b05d3
pipe2442 <felipeangulo2442@gmail.com>
2021-03-30 14:39:32 -0500

    Create User model - Create users controller - Add authentication and bootstrap views

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+    validates_uniqueness_of :email
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :users
+  resources :sessions
 end
