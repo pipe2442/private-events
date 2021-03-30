60d9bffba04302cbe0b93f58c85141190347e34f
pipe2442 <felipeangulo2442@gmail.com>
2021-03-30 14:45:30 -0500

    Add methods to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,28 @@
 class EventsController < ApplicationController
+    before_action :authorize, only: [:new, :edit, :update]
+    def index
+        @events = Event.all
+    end
+  
+    def show
+        @event = Event.find(params[:id])
+    end
+  
+    def new 
+        @event = current_user.events.new
+    end
+  
+    def create     
+        @event = current_user.events.new(event_params)
+        if @event.save
+          redirect_to root_path
+        else
+          render :new
+        end
+    end
+      
+    private
+        def event_params
+            params.require(:event).permit(:title, :description, :date, :place)
+        end
 end
