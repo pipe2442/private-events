db3e424d1a5d94bf074a755759014842f3550613
pipe2442 <felipeangulo2442@gmail.com>
2021-03-30 14:48:02 -0500

    Add users_params and show method to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,25 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-  end
-  
-  def create
-    @user = User.new(params[:user])
+end
+
+def show
+    @user = User.find(params[:id])
+end
+
+def create
+    @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signing up!"
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: "Thank you for signing up!"
     else
-      render "new"
+        render "new"
     end
-  end
+end
+
+private
+    def user_params
+        params.require(:user).permit(:email, :name)
+    end
   
 end
