86dc3755694eae1846a2fc1177152edc7511fba3
pipe2442 <felipeangulo2442@gmail.com>
2021-03-30 14:44:02 -0500

    Add event routes to config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/create'
+  # Session Routes
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  
+
+  root "events#index"
   resources :users
   resources :sessions
+  resources :events
 end
