7983e0450500c1c8496ebe74b2c23fdd6f80e6be
pipe2442 <felipeangulo2442@gmail.com>
2021-04-02 02:30:43 -0500

    Fix events_controller - Fix events titles to root specific event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,21 +5,23 @@ class EventsController < ApplicationController
     end
   
     def show
+        @users = User.all
         @event = Event.find(params[:id])
     end
   
     def new 
-        @event = current_user.events.new
+        @event = current_user.events.build
     end
   
     def create     
-        @event = current_user.events.new(event_params)
+        @event = current_user.events.build(event_params)
         if @event.save
           redirect_to root_path
         else
           render :new
         end
     end
+
       
     private
         def event_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  get 'attendances/new'
-  get 'attendances/create'
-  get 'attendance/new'
-  get 'attendance/create'
-  get 'events/index'
-  get 'events/show'
-  get 'events/new'
-  get 'events/create'
+
+  resources :users
+  resources :sessions
+
+  resources :events 
+
+  put 'event/:id/invitation', to: 'events#invitation', as: 'invitation'
+
+
   # Session Routes
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  root "events#index"
-  # resources :events do
-  #   resource :attendance 
-  # end
-  resources :users
   resources :sessions
-  resources :events
+  
+  root "events#index"
+
 end
