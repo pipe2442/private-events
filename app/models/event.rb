ebb39b977ecf0f6e03eebae832be037ff2fcdd76
pipe2442 <felipeangulo2442@gmail.com>
2021-04-05 17:30:24 -0500

    Add scopes for future and past events - Modified index logic to show events with scopes and .exist? ruby method

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User", foreign_key: "creator_id"
     has_many :attendances
     has_many :attendees, through: :attendances, source: :attendee
+
+    scope :past_events, -> { where("date < ?", Date.today) }
+    scope :future_events, -> { where("date > ?", Date.today) }
 end
