90bb1594248f26e2a3eff987337c85d2eedc29be
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-07 00:18:33 +0200

    Fix session testing feature

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # describe 'authentication' do
-  #   it 'http success status for signup' do
-  #     get '/users/new'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'authentication' do
+    it 'http success status for signup' do
+      get '/users/new'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
