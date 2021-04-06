b77ecfa05ed0c1a5c4fbf910a08d1bfd2cdbb0ab
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-07 00:18:19 +0200

    Fix session testing feature

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,29 +1,15 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-describe 'Signing in', type: :feature do
-  it 'tests if sign in is successful' do
-    user = User.new(name: 'Murilo', email: 'murilo@gmail.com')
-    user.save
-    visit '/login'
-    within('form') do
-      fill_in 'session_name', with: 'Murilo'
-    end
-    click_button 'commit'
-
-    expect(page).to have_content 'Welcome to Events planner, Murilo!'
-  end
-end
-
 RSpec.describe 'Signing up', type: :system do
-  # describe 'signup page is showing the right fields' do
-  #   it 'shows the username from signup page' do
-  #     visit '/users/new'
-  #     expect(page).to have_content 'Username'
-  #   end
-  #   it 'shows the email from signup page' do
-  #     visit '/users/new'
-  #     expect(page).to have_content 'E-mail'
-  #   end
-  # end
+  describe 'signup page is showing the right fields' do
+    it 'shows the Name from signup page' do
+      visit '/users/new'
+      expect(page).to have_content 'Name'
+    end
+    it 'shows the Email from signup page' do
+      visit '/users/new'
+      expect(page).to have_content 'Email'
+    end
+  end
 end
